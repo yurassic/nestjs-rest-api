beb40de627f31895ac9d47a27bab6dbd11c66c1e
Yuriy Nyay <ynyay@assurance.com>
2023-06-12 00:08:01 +0300

    Add CI checks

---
+++ quality-check.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Run linting tool
+npm run lint
+
+# Run unit tests
+npm run test
+
+# Check for vulnerabilities with npm audit
+npm audit
+
+# If any of the previous commands returned a non-zero exit code, report an error
+if [ $? -ne 0 ]; then
+  echo "The client app has problems with its quality."
+  exit 1
+fi
+
+# If all commands completed successfully, report success
+echo "The client app passed all quality checks."
